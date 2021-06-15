92332a9bae6a4560aa1c14af5cda482298b7fdcb
DeMarzoff <73641528+DeMarzoff@users.noreply.github.com>
2021-06-15 11:57:20 +0300

    Add files via upload

---
+++ main-buttons.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var buttons = document.getElementsByClassName('store-servers');
+
+  if (buttons.length > 0) {
+    Object.keys(buttons[0].children[0].children).forEach((key) => {
+      let button = buttons[0].children[0].children[key];
+      button.onclick = function(event) {
+        buttons[0].style.visibility = 'hidden';
+      }
+    });
+  }
+
+});
