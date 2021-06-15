2f6de8fdcd08a3e45ec36c58c5203e8569ebe4df
DeMarzoff <73641528+DeMarzoff@users.noreply.github.com>
2021-06-15 12:55:09 +0300

    Add files via upload

--- main-buttons.js
+++ main-buttons.js
@@ -1,5 +1,5 @@
 document.onclick = function(event) {
 	if (event.toElement.tagName.toLowerCase() == 'a' && event.toElement?.parentElement?.parentElement?.className.toLowerCase() == 'store-servers') {
-		event.toElement.parentElement.parentElement.style.visibility = 'hidden';
+		event.toElement.parentElement.parentElement.style.display = 'none';
 	}
 }
