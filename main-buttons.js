6aebce06dc229dcea0dd7e3123f3a7019313b7be
DeMarzoff <73641528+DeMarzoff@users.noreply.github.com>
2021-06-16 17:34:18 +0300

    Add files via upload

--- main-buttons.js
+++ main-buttons.js
@@ -1,4 +1,7 @@
-document.onclick = function(event) {
+document.onclick = MBInit;
+document.addEventListener("DOMContentLoaded", MBInit);
+document.addEventListener("load", MBInit);
+function MBInit(event) {
 	if (event.toElement.tagName.toLowerCase() == 'a' && event.toElement?.parentElement?.parentElement?.className.toLowerCase() == 'store-servers') {
 		event.toElement.parentElement.parentElement.style.display = 'none';
 	}
